2cb1aec96be773dcad47e97ed176d81befb63ede
danie odukwe <kingdanny295@gmail.com>
2024-02-02 12:15:52 +0100

    imported all the icons and images needed for navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,20 @@
+import { useState } from "react"
+import { logoBold } from '../assets'
 
+import { CiHeart, CiChat1, CiSearch } from "react-icons/ci";
+import { FiBell, FiShoppingCart } from "react-icons/fi";
+import { HiOutlineEnvelope } from "react-icons/hi2";
+import { TbGridDots } from "react-icons/tb";
 const Navbar = () => {
   return (
     <div>
       <nav className="w-full flex items-center justify-between py-8 px-4">
         <div>
           <div>
-            <img src="" alt="" />
+            <img src={logoBold} alt="friends_logo" />
+          </div>
+          <div>
+
           </div>
         </div>
       </nav>
