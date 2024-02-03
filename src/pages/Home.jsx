5f61e7a827f40b2ad76eec3bb504ed0f0b959b69
danie odukwe <kingdanny295@gmail.com>
2024-02-03 23:03:52 +0100

    done for today

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ import { BsThreeDots } from "react-icons/bs";
 import { HiOutlineDotsVertical } from "react-icons/hi";
 import { FaHeart } from "react-icons/fa6";
 
-import { jenna, smile, dan, david, edward, milly, daniel, elise, gaelle, nelly, stella, bobby, postImg1, postImg2, postImg3, postImg4, postImg5, postImg6, postImg7 } from '../assets'
+import { jenna, smile, dan, david, edward, milly, daniel, elise, gaelle, nelly, stella, bobby, postImg1, postImg2, postImg3, postImg4, postImg5, postImg6, postImg7, bootstrap } from '../assets'
 
 const Home = () => {
   return (
@@ -51,7 +51,7 @@ const Home = () => {
         </div>
       </div>
 
-      {/* POSTS */}
+      {/* POSTS 1*/}
       <div className="w-full bg-white flex flex-col gap-5 p-4 rounded-xl">
         <div className="flex items-center justify-between">
           <div className="flex items-center gap-3">
@@ -104,6 +104,158 @@ const Home = () => {
             <div className="flex items-center gap-1">
               <IoChatbubbleOutline className="text-[1.2rem]"/>
               <p>3</p>
+            </div>
+          </div>
+
+          {/* custom buttons */}
+          <div className="flex items-center gap-2 absolute bottom-[55px] right-0">
+            <div className="w-[45px] h-[45px] bg-customBlue rounded-full flex items-center justify-center text-white">
+              <IoChatbubbleOutline className="text-[1.2rem]"/>
+            </div>
+            <div className="w-[45px] h-[45px] bg-customBlue rounded-full flex items-center justify-center text-white">
+              <IoLinkOutline className="text-[1.2rem]"/>
+            </div>
+            <div className="peer w-[45px] h-[45px] bg-white rounded-full flex items-center justify-center border text-red-500 hover:bg-red-500 hover:text-white ">
+              <FaHeart/>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* POSTS 2*/}
+      <div className="w-full bg-white flex flex-col gap-5 p-4 rounded-xl">
+        <div className="flex items-center justify-between">
+          <div className="flex items-center gap-3">
+            <img src={edward} alt="edward" className="w-[40px] rounded-full"/>
+            <div className="flex flex-col gap-0 ">
+              <h1 className="text-[0.85rem] font-medium">Edward Mayers</h1>
+              <p className="text-[0.8rem]">july 26 2022, 01:03pm</p>
+            </div>
+          </div>
+          <div>
+            <HiOutlineDotsVertical />
+          </div>
+        </div>
+
+        {/* article */}
+        <p className="text-[0.9rem]">
+        You all know how i love bootstrap, but i didn't have time to dig deeper into it. Therefore i found this very interesting video i wanted to share with you all. <span className="font-medium">#bootsrap #webdesign</span>
+        </p>
+        
+        {/* image */}
+        <div className="w-full flex items-start gap-3 bg-whiteBg p-3">
+          <img src={bootstrap} alt="bootstrap" className="rounded-xl h-[120px] w-[250px] object-cover"/>
+          <div className="flex flex-col gap-2">
+            <h1 className="text-[1rem] font-medium">
+              What's new in Bootstrap 4 ?
+            </h1>
+            <p className="text-[0.85rem]">
+              Before I create the new Bootstrap 4 crash course I want to go over some of the changes from Bootstrap 3.
+            </p>
+            <p className="text-[0.75rem]">
+              YOUTUBE.COM
+            </p>
+          </div>
+        </div>
+        
+        {/* INTERRACTIONS */}
+        <div className="flex items-center w-full relative justify-between py-2">
+          <div className="flex items-center gap-[45px]">
+            <div className="flex items-center gap-3 relative">
+              <img src={daniel} alt="daniel" className="w-[35px] h-[35px] rounded-full"/>
+              <img src={elise} alt="elsie" className="w-[35px] h-[35px] rounded-full absolute left-6"/>
+            </div>
+
+            <div className="flex flex-col gap-0">
+              <h1 className="text-[0.8rem] font-medium">Daniel, Elise</h1>
+              <p className="text-[0.8rem]"> liked this</p>
+            </div>
+          </div>
+
+          <div className="flex items-center gap-3 text-[0.9rem]">
+            <div className="flex items-center gap-1">
+              <CiHeart className="text-[1.3rem]"/>
+              <p>2</p>
+            </div>
+
+            <div className="flex items-center gap-1">
+              <IoLinkOutline className="text-[1.3rem]"/>
+              <p>0</p>
+            </div>
+            
+            <div className="flex items-center gap-1">
+              <IoChatbubbleOutline className="text-[1.2rem]"/>
+              <p>2</p>
+            </div>
+          </div>
+
+          {/* custom buttons */}
+          <div className="flex items-center gap-2 absolute bottom-[55px] right-0">
+            <div className="w-[45px] h-[45px] bg-customBlue rounded-full flex items-center justify-center text-white">
+              <IoChatbubbleOutline className="text-[1.2rem]"/>
+            </div>
+            <div className="w-[45px] h-[45px] bg-customBlue rounded-full flex items-center justify-center text-white">
+              <IoLinkOutline className="text-[1.2rem]"/>
+            </div>
+            <div className="peer w-[45px] h-[45px] bg-white rounded-full flex items-center justify-center border text-red-500 hover:bg-red-500 hover:text-white ">
+              <FaHeart/>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* POSTS 3*/}
+      <div className="w-full bg-white flex flex-col gap-5 p-4 rounded-xl">
+        <div className="flex items-center justify-between">
+          <div className="flex items-center gap-3">
+            <img src={elise} alt="elise" className="w-[40px] rounded-full"/>
+            <div className="flex flex-col gap-0 ">
+              <h1 className="text-[0.85rem] font-medium">Elise Walker</h1>
+              <p className="text-[0.8rem]">july 26 2022, 01:03pm</p>
+            </div>
+          </div>
+          <div>
+            <HiOutlineDotsVertical />
+          </div>
+        </div>
+
+        {/* article */}
+        <p className="text-[0.9rem]">
+        Thanks a lot to <span className="font-medium">@Gaelle</span> and <span className="font-medium">@Rolf</span> for this wonderful team lunch. The food was really tasty and we had some great laughs. Thanks to all the team, you're all awesome !
+        </p>
+        
+        {/* image */}
+        <img src={postImg2} alt="image-2" className="rounded-xl"/>
+
+        {/* INTERRACTIONS */}
+        <div className="flex items-center w-full relative justify-between py-2">
+          <div className="flex items-center gap-[70px]">
+            <div className="flex items-center gap-3 relative">
+              <img src={gaelle} alt="gaelle" className="w-[35px] h-[35px] rounded-full"/>
+              <img src={edward} alt="edward" className="w-[35px] h-[35px] rounded-full absolute left-6"/>
+              <img src={nelly} alt="nelly" className="w-[35px] h-[35px] rounded-full absolute left-[50px]"/>
+            </div>
+
+            <div className="flex flex-col gap=0">
+              <h1 className="text-[0.8rem] font-medium">Gaelle, Edward</h1>
+              <p className="text-[0.8rem]">and 1 more liked this</p>
+            </div>
+          </div>
+
+          <div className="flex items-center gap-3 text-[0.9rem]">
+            <div className="flex items-center gap-1">
+              <CiHeart className="text-[1.3rem]"/>
+              <p>3</p>
+            </div>
+
+            <div className="flex items-center gap-1">
+              <IoLinkOutline className="text-[1.3rem]"/>
+              <p>0</p>
+            </div>
+            
+            <div className="flex items-center gap-1">
+              <IoChatbubbleOutline className="text-[1.2rem]"/>
+              <p>5</p>
             </div>
           </div>
 
