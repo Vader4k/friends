225e9474718d3618d358bf4f905ddb208e61ca9c
danie odukwe <kingdanny295@gmail.com>
2024-02-03 02:04:00 +0100

    modified constant data

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,45 +1,80 @@
-import { bobby, dan, nelly, milly, elise, edward } from '../assets'
+import { feed, friends, videos, euro, cart, house, timer, question, news, cake, envato, calendar, pin, idea, settings} from '../assets'
 
-import { IoPersonRemoveOutline, IoPersonAddOutline  } from "react-icons/io5";
-import { GoTag } from "react-icons/go";
-export const friendOpen = [
+export const explorePage = [
     {
-        name: 'Bobby Brown',
-        update: 'Najeel verwick is a common friend',
-        friend: <IoPersonAddOutline /> ,
-        unfriend: <IoPersonRemoveOutline />,
-        img: bobby
+        id: 1,
+        img: feed,
+        tag: 'Feed'
     },
     {
-        name: 'Dan Walker',
-        update: 'You have 4 common friends ',
-        friend: <IoPersonAddOutline />,
-        unfriend: <IoPersonRemoveOutline />,
-        img: dan
+        id: 2,
+        img: friends,
+        tag: 'Friends'
     },
     {
-        name: 'Nelly Schwartz',
-        update: 'You are now friends with nelly schwatz. Check her profile',
-        tag: <GoTag />,
-        img: nelly
+        id: 3,
+        img: videos,
+        tag: 'Feed'
     },
     {
-        name: 'Milly Augustine',
-        update: 'Najeel verwick is a common friend',
-        friend: <IoPersonAddOutline />,
-        unfriend: <IoPersonRemoveOutline />,
-        img: milly
+        id: 4,
+        img: euro,
+        tag: 'pages'
     },
     {
-        name: 'Elise Walker',
-        update: 'You are now friends with Elise walker, Check her profile',
-        tag: <GoTag />,
-        img: elise
+        id: 5,
+        img: cart,
+        tag: 'Commerce'
     },
     {
-        name: 'Edward Mayers',
-        update: 'You are now friends with Edward Mayers. Check his profile',
-        tag: <GoTag />,
-        img: edward
-    }
+        id: 6,
+        img: house,
+        tag: 'Interest'
+    },
+    {
+        id: 7,
+        img: timer,
+        tag: 'Stories'
+    },
+    {
+        id: 8,
+        img: question,
+        tag: 'Questions'
+    },
+    {
+        id: 9,
+        img: news,
+        tag: 'News'
+    },
+    {
+        id: 10,
+        img: cake,
+        tag: 'Group'
+    },
+    {
+        id: 11,
+        img: envato,
+        tag: 'Ento'
+    },
+    {
+        id: 12,
+        img: calendar,
+        tag: 'Events'
+    },
+    {
+        id: 13,
+        img: pin,
+        tag: 'Css Ninja'
+    },
+    {
+        id: 14,
+        img: idea,
+        tag: 'Elements'
+    },
+    {
+        id: 15,
+        img: settings,
+        tag: 'Settings'
+    },
+
 ]
